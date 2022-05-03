0ce1ab8d10b0e5c40544e676c002f5782728fd17
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-05-03 18:26:14 +0000

    new

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,27 +1,32 @@
-import { useState } from 'react'
-import { SendOutlined, PictureOutlined } from '@ant-design/icons'
-import { sendMessage, isTyping } from 'react-chat-engine'
+import { useState } from 'react';
+import { SendOutlined, PictureOutlined } from '@ant-design/icons';
+import { sendMessage, isTyping } from 'react-chat-engine';
 
 const MessageForm = (props) => {
-  const [value, setValue] = useState('')
-  const { chatId, creds } = props
+  const [value, setValue] = useState('');
+  const { chatId, creds } = props;
 
   const handleChange = (event) => {
-    setValue(event.target.value)
-    isTyping(props, chatId)
-  }
+    setValue(event.target.value);
+
+    isTyping(props, chatId);
+  };
 
   const handleSubmit = (event) => {
-    event.preventDefault()
-    const text = value.trim()
+    event.preventDefault();
+
+    const text = value.trim();
+
     if (text.length > 0) {
-      sendMessage(creds, chatId, { text })
+      sendMessage(creds, chatId, { text });
     }
-    setValue('')
-  }
+
+    setValue('');
+  };
+
   const handleUpload = (event) => {
-    sendMessage(creds, chatId, { files: event.target.files, text: '' })
-  }
+    sendMessage(creds, chatId, { files: event.target.files, text: '' });
+  };
 
   return (
     <form className="message-form" onSubmit={handleSubmit}>
@@ -48,7 +53,7 @@ const MessageForm = (props) => {
         <SendOutlined className="send-icon" />
       </button>
     </form>
-  )
-}
+  );
+};
 
-export default MessageForm
+export default MessageForm;
--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -7,22 +7,14 @@ const MyMessage = ({ message }) => {
         className="message-image"
         style={{ float: 'right' }}
       />
-    )
+    );
   }
 
   return (
-    <div
-      className="message"
-      style={{
-        float: 'right',
-        marginRight: '18px',
-        color: 'white',
-        backgroundColor: '#3B2A50',
-      }}
-    >
+    <div className="message" style={{ float: 'right', marginRight: '18px', color: 'white', backgroundColor: '#3B2A50' }}>
       {message.text}
     </div>
-  )
-}
+  );
+};
 
-export default MyMessage
+export default MyMessage;
