5ab5674d8d3fef48496b83ffc8a3538d8140e08d
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-05-03 18:25:27 +0000

    new

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import React from 'react'
 import { ChatEngine } from 'react-chat-engine'
+
 import ChatFeed from './components/ChatFeed'
+import LoginForm from './components/LoginForm'
 import './App.css'
 
+const projectID = '4e4b1da0-06b7-4fa8-b9a3-31a08f615b08'
+
 const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />
+
   return (
-    <>
-      <ChatEngine
-        height="100vh"
-        projectID="4e4b1da0-06b7-4fa8-b9a3-31a08f615b08"
-        userName="ayoub"
-        userSecret="lina2015"
-        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-      />
-    </>
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() =>
+        new Audio(
+          'https://chat-engine-assets.s3.amazonaws.com/click.mp3'
+        ).play()
+      }
+    />
   )
 }
 
